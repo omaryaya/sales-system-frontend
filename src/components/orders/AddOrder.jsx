acc5eab1bfe106a2f31702aeb434d0c75f6165de
Omar YAYA <47383263+omaryaya@users.noreply.github.com>
2020-10-10 15:37:37 +0200

    commit new branch

--- src/components/orders/AddOrder.jsx
+++ src/components/orders/AddOrder.jsx
@@ -7,7 +7,7 @@ import { getProductsList } from '../../actions/products';
 import { Grid, TextField, Button } from '@material-ui/core';
 
 
-import { makeStyles/* , useTheme */ } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
